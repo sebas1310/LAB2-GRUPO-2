f4f295a738399b1061c67dce550ed5ee439b7164
sebas1310 <a20203369@pucp.edu.pe>
2023-04-04 21:43:00 -0500

    proveedor edit

--- src/main/java/com/example/lab2grupo2/controller/ProveedorController.java
+++ src/main/java/com/example/lab2grupo2/controller/ProveedorController.java
@@ -1,9 +1,14 @@
 package com.example.lab2grupo2.controller;
 
+import com.example.lab2grupo2.entity.Proveedor;
 import com.example.lab2grupo2.repository.ProveedorRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class ProveedorController {
@@ -18,5 +23,24 @@ public class ProveedorController {
     public String listaProveedor(Model model){
         model.addAttribute("list",proveedorRepository.findAll());
         return "proveedor/lista";
+    }
+    @GetMapping(value = "/proveedor/editar")
+    public String editaDistribuidoras(@RequestParam("id") Integer id, Model model){
+        Optional<Proveedor> optionalDistributors= proveedorRepository.findById(id);
+        if(optionalDistributors.isPresent()){
+            Proveedor proveedor = optionalDistributors.get();
+            model.addAttribute("prov", proveedor);
+            return "proveedor/edit";
+        }else {
+            return "redirect:/proveedor/lista";
+        }
+
+    }
+
+
+    @PostMapping(value = "proveedor/guardar")
+    public String guardar(Proveedor proveedor){
+        proveedorRepository.save(proveedor);
+        return "redirect:/proveedor/lista";
     }
 }
