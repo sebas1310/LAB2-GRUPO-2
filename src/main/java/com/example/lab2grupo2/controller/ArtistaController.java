bfb6ce84d471c3e544c8eb7847f91fb01d51e576
alexia2212 <alexia_jg@outlook.es>
2023-04-04 21:17:25 -0500

    artista controller class

--- src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
+++ src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
@@ -1,5 +1,6 @@
 package com.example.lab2grupo2.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.example.lab2grupo2.entity.Artista;
 import com.example.lab2grupo2.repository.ArtistaRepository;
 import com.example.lab2grupo2.repository.ProveedorRepository;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/artistas")
@@ -19,8 +24,8 @@ public class ArtistaController {
     }
 
     @GetMapping("/listar")
-    public String listar(Artista artista) {
-
+    public String listar(Model model) {
+        List<Artista> artistaList = artistaRepository.findAll();
         return "shipper/lista";
     }
     
@@ -32,4 +37,27 @@ public class ArtistaController {
         return "artista/lista";
     }
 
+
+
+    @GetMapping("/editar")
+    public String editar(@RequestParam("id") int id, Artista artista) {
+
+        Optional<Artista> shipperOptional = artistaRepository.findById(id);
+        if (shipperOptional.isPresent()) {
+            Artista artista1 = shipperOptional.get();
+            artista1.addAttribute("shipper", artista1);
+            return "shipper/editarForm";
+        } else {
+            return "redirect:/listar";
+        }
+    }
+
+    @GetMapping("/borrar")
+    public String borrar(@RequestParam("id") int id) {
+        Optional<Artista> shipperOptional = artistaRepository.findById(id);
+        if (shipperOptional.isPresent()) {
+            artistaRepository.deleteById(id);
+        }
+        return "redirect:/listar";
+    }
 }
