5c8b7de47ba7d15383afaa8b49995308fd9de4b6
sebas1310 <a20203369@pucp.edu.pe>
2023-04-04 20:42:30 -0500

    proyecto

---
+++ src/main/java/com/example/lab2grupo2/Lab2Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Grupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Grupo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2grupo2/Lab2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Grupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
