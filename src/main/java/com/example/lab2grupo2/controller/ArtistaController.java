cd943036a976fc0fccc27ba7bf03adf3a347af77
alexia2212 <alexia_jg@outlook.es>
2023-04-04 20:58:52 -0500

    lista de artistas

--- src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
+++ src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
@@ -1,4 +1,14 @@
 package com.example.lab2grupo2.controller;
 
+import com.example.lab2grupo2.repository.ProveedorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ArtistaController {
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+
+        model.addAttribute("lista", ProveedorRepository."metodo"());
+        return "shipper/lista";
+    }
 }
---
+++ src/main/java/com/example/lab2grupo2/entity/Artista.java
@@ -0,0 +1,4 @@
+package com.example.lab2grupo2.entity;
+
+public class Artista {
+}
