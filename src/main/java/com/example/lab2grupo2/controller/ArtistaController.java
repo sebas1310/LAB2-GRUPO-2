52734a5f41b3b691e00b6c58923b3949e678d9d3
alexia2212 <alexia_jg@outlook.es>
2023-04-04 21:10:27 -0500

    pom para repository y mysql

--- src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
+++ src/main/java/com/example/lab2grupo2/controller/ArtistaController.java
@@ -1,14 +1,35 @@
 package com.example.lab2grupo2.controller;
 
+import com.example.lab2grupo2.entity.Artista;
+import com.example.lab2grupo2.repository.ArtistaRepository;
 import com.example.lab2grupo2.repository.ProveedorRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/artistas")
 public class ArtistaController {
+    final ArtistaRepository artistaRepository;
+
+    public ArtistaController(ArtistaRepository artistaRepository) {
+        this.artistaRepository = artistaRepository;
+    }
 
     @GetMapping("/listar")
-    public String listar(Model model) {
+    public String listar(Artista artista) {
 
-        model.addAttribute("lista", ProveedorRepository."metodo"());
+
         return "shipper/lista";
     }
+
+
+    @PostMapping("/guardar")
+    public String guardar(Artista artista) {
+
+        artistaRepository.save(artista);
+        return "artista/lista";
+    }
+
 }
