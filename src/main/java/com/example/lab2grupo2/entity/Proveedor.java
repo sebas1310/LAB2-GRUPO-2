33c66aa5ea58ec635e5b8104324fd989cae866d2
sebas1310 <a20203369@pucp.edu.pe>
2023-04-04 21:02:12 -0500

    vistas controller etc

--- src/main/java/com/example/lab2grupo2/entity/Proveedor.java
+++ src/main/java/com/example/lab2grupo2/entity/Proveedor.java
@@ -1,4 +1,27 @@
 package com.example.lab2grupo2.entity;
 
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "proveedores")
 public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idProveedores")
+    private Integer id;
+
+    @Column(name = "Nombre")
+    private String nombre;
+
+    @Column(name = "Telefono")
+    private Integer telefono;
+
+    @Column(name = "Elemento")
+    private String elemento;
+
 }
--- src/main/java/com/example/lab2grupo2/repository/ProveedorRepository.java
+++ src/main/java/com/example/lab2grupo2/repository/ProveedorRepository.java
@@ -1,4 +1,7 @@
 package com.example.lab2grupo2.repository;
 
-public interface ProveedorRepository {
+import com.example.lab2grupo2.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedorRepository extends JpaRepository<Proveedor,Integer> {
 }
