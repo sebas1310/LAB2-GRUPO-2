903b3092ac45508dbf5819d5d133f13eb052d3f6
alexia2212 <alexia_jg@outlook.es>
2023-04-04 21:52:23 -0500

    artista editar

--- src/main/java/com/example/lab2grupo2/repository/ArtistaRepository.java
+++ src/main/java/com/example/lab2grupo2/repository/ArtistaRepository.java
@@ -3,8 +3,7 @@ package com.example.lab2grupo2.repository;
 import com.example.lab2grupo2.entity.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-/*
+
 @Repository
 public interface ArtistaRepository extends JpaRepository<Artista,Integer> {
 }
-*/
