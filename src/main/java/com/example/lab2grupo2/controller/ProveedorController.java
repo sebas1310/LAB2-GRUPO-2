369ecd9c75ed9059b832ed082b7bde7eeea00cbe
sebas1310 <a20203369@pucp.edu.pe>
2023-04-04 21:28:20 -0500

    proveedor

--- src/main/java/com/example/lab2grupo2/controller/ProveedorController.java
+++ src/main/java/com/example/lab2grupo2/controller/ProveedorController.java
@@ -1,8 +1,22 @@
 package com.example.lab2grupo2.controller;
 
+import com.example.lab2grupo2.repository.ProveedorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProveedorController {
 
+    final ProveedorRepository proveedorRepository;
+
+    public ProveedorController(ProveedorRepository proveedorRepository) {
+        this.proveedorRepository = proveedorRepository;
+    }
+
+    @GetMapping(value = "/proveedor/lista")
+    public String listaProveedor(Model model){
+        model.addAttribute("list",proveedorRepository.findAll());
+        return "proveedor/lista";
+    }
 }
